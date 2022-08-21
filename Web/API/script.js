ca0c5d3014202cd5dab3ba4442496b3f2fb6806c
prashant_kr <prashant.kumar2_cs19@gla.ac.in>
2022-08-22 02:09:19 +0530

    adding new file

--- Web/API/script.js
+++ Web/API/script.js
@@ -3,5 +3,5 @@ const dogImageDiv = document.getElementById('dogImage')
 fetch('https://dog.ceo/api/breeds/image/random ')
  .then(response=>response.json())
  .then(json=>{console.log(json.message)
-  dogImageDiv.innerHTML = `<img src='${json.message}' height=30% width = 50%px/>`
+  dogImageDiv.innerHTML = `<img src='${json.message}' height=500px width = 500px/>`
 })
