ffa2474dc7595970dab933e46ea6c8f0057dfa7f
prashant_kr <prashant.kumar2_cs19@gla.ac.in>
2022-08-22 01:59:24 +0530

    Adding button

--- Web/API/script.js
+++ Web/API/script.js
@@ -1,3 +1,7 @@
-fetch(' https://dog.ceo/api/breeds/image/random')
-.then(response=>response.json())
-.then(json=>console.log(json))
+
+const dogImageDiv = document.getElementById('dogImage')
+fetch('https://dog.ceo/api/breeds/image/random ')
+ .then(response=>response.json())
+ .then(json=>{console.log(json.message)
+  dogImageDiv.innerHTML = `<img src='${json.message}' height=30% width = 50%px/>`
+})
